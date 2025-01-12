04435469d4a5025d9b8e9ea0db5d6b5abd9e3ffd
atrocityz <disbalance17@Mail.ru>
2025-01-13 02:24:41 +0700

    add: blog-details and about sections implementation for blog page

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,11 @@ import Header from "./Header.js";
 import TabsCollection from "./Tabs.js";
 import defineScrollBarWidthCSSVar from './utils/defineScrollBarWidthCSSVar.js'
 import VideoPlayerCollection from "./VideoPlayer.js";
+import ExpandableContentCollection from "./ExpandableContent.js";
 
 new Header();
 new TabsCollection();
 new VideoPlayerCollection();
+new ExpandableContentCollection();
 
 defineScrollBarWidthCSSVar();
